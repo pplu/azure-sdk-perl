cf0f6bc7b9739029643fb9aa952396093d02a07f
Jose Luis Martinez <jlmartinez@capside.com>
2016-05-09 21:52:54 +0000

    types on properties of return objects

--- builder-lib/Azure/SDK/Builder/Property.pm
+++ builder-lib/Azure/SDK/Builder/Property.pm
@@ -4,4 +4,23 @@ package Azure::SDK::Builder::Property;
 
   has name => (is => 'ro', isa => 'Str');
 
+
+  has perl_type => (
+    is => 'ro',
+    isa => 'Str',
+    default => sub {
+      my $self = shift;
+      if      ($self->type eq 'string') {
+        return 'Str';
+      } elsif ($self->type eq 'array') {
+        #TODO: find out about the inner type for the array
+        return 'ArrayRef';
+      } else {
+        use Data::Dumper;
+        print Dumper($self);
+        warn 'Can\'t find a Perl type for ' . $self->type;
+        return 'Any'
+      }
+    }
+  );
 1;
