331f25552ac79f1f24e3b2983d69a0709e6fcfbe
Jose Luis Martinez <jlmartinez@capside.com>
2016-05-10 20:08:06 +0000

    Datatype for args objects

--- builder-lib/Azure/SDK/Builder/Parameter.pm
+++ builder-lib/Azure/SDK/Builder/Parameter.pm
@@ -16,4 +16,26 @@ package Azure::SDK::Builder::Parameter;
     }
   );
 
+  has type => (is => 'ro', isa => 'Str');
+
+  has perl_type => (
+    is => 'ro',
+    isa => 'Str',
+    lazy => 1,
+    default => sub {
+      my $self = shift;
+      if      ($self->type eq 'string') {
+        return 'Str';
+      } elsif ($self->type eq 'array') {
+        #TODO: find out about the inner type for the array
+        return 'ArrayRef';
+      } else {
+        use Data::Dumper;
+        print Dumper($self);
+        warn 'Can\'t find a Perl type for ' . $self->type;
+        return 'Any'
+      }
+    }
+  );
+
 1;
