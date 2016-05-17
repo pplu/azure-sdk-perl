1c2dc9386b27e605f1c125c7915e27d2df6072ad
Jose Luis Martinez <jlmartinez@capside.com>
2016-05-17 21:40:52 +0000

    Give types to arrays and objects

--- builder-lib/Azure/SDK/Builder/Property.pm
+++ builder-lib/Azure/SDK/Builder/Property.pm
@@ -3,6 +3,7 @@ package Azure::SDK::Builder::Property;
   extends 'Swagger::Schema::Schema';
 
   has name => (is => 'ro', isa => 'Str');
+  has ref => (is => 'ro', isa => 'Str');
 
   has root_schema => (
     is => 'ro',
@@ -14,16 +15,29 @@ package Azure::SDK::Builder::Property;
   has perl_type => (
     is => 'ro',
     isa => 'Str',
+    lazy => 1,
     default => sub {
       my $self = shift;
-      if      ($self->type eq 'string') {
+
+      if      (not defined $self->type){
+        my (undef, $second) = $self->root_schema->path_parts($self->ref);
+        return $self->root_schema->namespace($second);
+      } elsif ($self->type eq 'string') {
         return 'Str';
-      } elsif ($self->type eq 'array') {
+      } elsif ($self->type eq 'array' or defined $self->items) {
         #TODO: find out about the inner type for the array
-        return 'ArrayRef';
+        my $inner;
+        if (defined $self->items->ref) {
+          my (undef, $second) = $self->root_schema->path_parts($self->items->ref);
+          $inner = $self->root_schema->namespace($second);
+        } else {
+          die "Here we should be detecting a native type";
+        }
+
+        return "ArrayRef[$inner]";
       } else {
         use Data::Dumper;
-        print Dumper($self);
+        print Dumper({ %$self, root_schema => undef });
         warn 'Can\'t find a Perl type for ' . $self->type;
         return 'Any'
       }
