21c2f7832538ac8a959ddf8d979ba59ed9c51582
Oriol Soriano <oriol.soriano@capside.com>
2016-09-22 12:31:54 +0200

    Example to list nics after namespacing Azure classes to 'Azure::'

--- examples/nic_list.pl
+++ examples/nic_list.pl
@@ -1,16 +1,14 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
 
 use Azure;
-use Feature;
-use NetworkResourceProvider;
+use Azure::NetworkResourceProvider;
+use Azure::Credentials::AzureADClientCredentials;
 
 use Azure::Net::Caller;
 
-my $r = NetworkResourceProvider->new(
-  caller => Azure::Net::Caller->new,
+my $azure = Azure->new(
   credentials => Azure::Credentials::AzureADClientCredentials->new(
     tenant_id => $ENV{AZURE_TENANT_ID},
     client_id => $ENV{AZURE_CLIENT_ID},
@@ -18,7 +16,8 @@ my $r = NetworkResourceProvider->new(
   ),
 );
 
-my $ret = $r->ListNetworkInterfaces(
+my $nw  = $azure->service('NetworkResourceProvider');
+my $ret = $nw->ListNetworkInterfaces(
   'api-version'  => '2016-03-30',
   subscriptionId => $ENV{AZURE_SUBSCRIPTION_ID},
   resourceGroupName => 'ENZIMETEST',
