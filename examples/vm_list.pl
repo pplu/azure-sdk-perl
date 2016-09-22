c5ac9c953457c917003f504e57f890ba74d86b80
Oriol Soriano <oriol.soriano@capside.com>
2016-09-22 18:34:27 +0200

    Takes into account new Azure namespace

--- examples/vm_list.pl
+++ examples/vm_list.pl
@@ -1,15 +1,13 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
 
 use Azure;
-use ComputeManagement;
+use Azure::Credentials::AzureADClientCredentials;
 
-use Azure::Net::Caller;
 
-my $r = ComputeManagement->new(
-  caller => Azure::Net::Caller->new,
+
+my $azure = Azure->new(
   credentials => Azure::Credentials::AzureADClientCredentials->new(
     tenant_id => $ENV{AZURE_TENANT_ID},
     client_id => $ENV{AZURE_CLIENT_ID},
@@ -17,7 +15,8 @@ my $r = ComputeManagement->new(
   ),
 );
 
-my $ret = $r->ListVirtualMachines(
+my $c   = $azure->service('ComputeManagement');
+my $ret = $c->ListVirtualMachines(
   'api-version'  => '2016-03-30',
   subscriptionId => $ENV{AZURE_SUBSCRIPTION_ID},
   resourceGroupName => 'ENZIMETEST',
