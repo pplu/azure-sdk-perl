fc3ea428cdd966a9dd1d5c73eab7c6dfa07b01f8
Oriol Soriano <oriol.soriano@capside.com>
2016-09-28 10:25:46 +0200

    Adds 'properties' attr to VirtualMachine resource definition

--- auto-lib/Azure/ComputeManagement/VirtualMachine.pm
+++ auto-lib/Azure/ComputeManagement/VirtualMachine.pm
@@ -17,5 +17,6 @@ package Azure::ComputeManagement::VirtualMachine;
   has 'location' => (is => 'ro', isa => 'Str'  );
   has 'name' => (is => 'ro', isa => 'Str'  );
   has 'tags' => (is => 'ro', isa => 'HashRef'  );
+  has 'properties' => (is => 'ro', isa => 'HashRef'  );
   has 'type' => (is => 'ro', isa => 'Str'  );
 1;
