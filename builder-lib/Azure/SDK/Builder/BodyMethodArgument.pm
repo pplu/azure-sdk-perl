5973997f2b3a2317badc745f09d1e92cca7b9662
Jose Luis Martinez <jlmartinez@capside.com>
2016-10-05 20:05:39 +0200

    Silence warnings while finding types. Better diagnostics

--- builder-lib/Azure/SDK/Builder/BodyMethodArgument.pm
+++ builder-lib/Azure/SDK/Builder/BodyMethodArgument.pm
@@ -53,23 +53,29 @@ package Azure::SDK::Builder::BodyMethodArgument;
     lazy => 1,
     default => sub {
       my $self = shift;
-      use Data::Printer;
-      p $self if (not defined $self->type);
-      if      ($self->type eq 'string') {
-        return 'Str';
-      } elsif ($self->type eq 'integer') {
-        return 'Int';
-      } elsif ($self->type eq 'array') {
-        #TODO: find out about the inner type for the array
-        return 'ArrayRef';
-      } elsif ($self->type eq 'object') {
-        #TODO: additionalProperties->type should contain the
-        # type of the values of the hashref
-        return 'HashRef';
+
+      if (defined $self->type) {
+        if      ($self->type eq 'string') {
+          return 'Str';
+        } elsif ($self->type eq 'integer') {
+          return 'Int';
+        } elsif ($self->type eq 'array') {
+          #TODO: find out about the inner type for the array
+          return 'ArrayRef';
+        } elsif ($self->type eq 'object') {
+          #TODO: additionalProperties->type should contain the
+          # type of the values of the hashref
+          return 'HashRef';
+        } else {
+          use Data::Dumper;
+          print Dumper({ %$self, root_schema => undef });
+          warn 'Can\'t find a Perl type for ' . $self->type . ' in ' . $self->parameter_name;
+          return 'Any'
+        }
       } else {
         use Data::Dumper;
         print Dumper({ %$self, root_schema => undef });
-        warn 'Can\'t find a Perl type for ' . $self->type . ' in ' . $self->parameter_name;
+        warn 'Can\'t find a Perl type because self->type is undefined in ' . $self->parameter_name;
         return 'Any'
       }
     }
