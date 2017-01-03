fbe0a0f314427cafeacb2f3b712c43ec298ce133
Jose Luis Martinez <jlmartinez@capside.com>
2017-01-03 16:27:24 +0000

    Delete an attempt to create a custom Insights client

--- lib/Insights/LocalizableString.pm
+++
@@ -1,7 +0,0 @@
-package Insights::LocalizableString;
-  use Moose;
-
-  has value => (is => 'ro', isa => 'Str');
-  has localizedValue => (is => 'ro', isa => 'Str');
-
-1;
--- lib/Insights/MetricDefinition.pm
+++
@@ -1,9 +0,0 @@
-package Insights::MetricDefinition;
-  use Moose;
-
-  has name => (is => 'ro', isa => 'Insights::LocalizableString');
-  has unit => (is => 'ro', isa => 'Str');
-  has primaryAggregationType => (is => 'ro', isa => 'Str');
-
-
-1;
