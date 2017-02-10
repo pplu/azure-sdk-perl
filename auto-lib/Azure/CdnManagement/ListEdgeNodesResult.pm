afb7c76770f8e063a73bbdef28c12e3ce4a8ae8c
Jose Luis Martinez <jlmartinez@capside.com>
2017-02-10 22:16:01 +0100

    CfnManagement API update

--- auto-lib/Azure/CdnManagement/ListEdgeNodesResult.pm
+++ auto-lib/Azure/CdnManagement/ListEdgeNodesResult.pm
@@ -1,6 +1,7 @@
 package Azure::CdnManagement::ListEdgeNodesResult;
   use Moose;
 
+  has nextLink => (is => 'ro', isa => 'Str'  );
   has value => (is => 'ro', isa => 'ArrayRef[Azure::CdnManagement::EdgeNode]'  );
 
 1;
