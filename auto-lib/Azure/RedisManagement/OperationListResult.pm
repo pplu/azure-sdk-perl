e6344164f25af36a0334956a9ad7267bd32d662a
Jose Luis Martinez <jlmartinez@capside.com>
2017-03-15 22:59:34 +0100

    RedisManagement API update

---
+++ auto-lib/Azure/RedisManagement/OperationListResult.pm
@@ -0,0 +1,6 @@
+package Azure::RedisManagement::OperationListResult;
+  use Moose;
+
+  has 'nextLink' => (is => 'ro', isa => 'Str'  );
+  has 'value' => (is => 'ro', isa => 'ArrayRef[Azure::RedisManagement::Operation]'  );
+1;
