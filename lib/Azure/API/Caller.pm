2f7ec9b96db1235773fe33a24aeeb89eef7686b8
Jose Luis Martinez <jlmartinez@capside.com>
2017-05-08 16:10:17 +0000

    Rename azure CLI to azure-perl (so it doesn't clash with the official command)

--- lib/Azure/API/Caller.pm
+++ lib/Azure/API/Caller.pm
@@ -221,4 +221,38 @@ package Azure::API::Caller;
     $class->new(%args);
     }
   }
+
+  sub to_hash {
+    my ($self, $params) = @_;
+    my $refHash = {};
+
+    if      ($params->does('Paws::API::StrToNativeMapParser')) {
+      return $params->Map;
+    } elsif ($params->does('Paws::API::StrToObjMapParser')) {
+      return { map { ($_ => $self->to_hash($params->Map->{$_})) } keys %{ $params->Map } };
+    }
+
+    foreach my $att (grep { $_ !~ m/^_/ } $params->meta->get_attribute_list) {
+      my $key = $att;
+      if (defined $params->$att) {
+        my $att_type = $params->meta->get_attribute($att)->type_constraint;
+        if ($att_type eq 'Bool') {
+          $refHash->{ $key } = ($params->$att)?1:0;
+        } elsif ($self->_is_internal_type($att_type)) {
+          $refHash->{ $key } = $params->$att;
+        } elsif ($att_type =~ m/^ArrayRef\[(.*)\]/) {
+          if ($self->_is_internal_type("$1")){
+            $refHash->{ $key } = $params->$att;
+          } else {
+            $refHash->{ $key } = [ map { $self->to_hash($_) } @{ $params->$att } ];
+          }
+        } elsif ($att_type->isa('Moose::Meta::TypeConstraint::Enum')) {
+          $refHash->{ $key } = $params->$att;
+        } else {
+          $refHash->{ $key } = $self->to_hash($params->$att);
+        }
+      }
+    }
+    return $refHash;
+  }
 1;
