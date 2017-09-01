94c768c894587328d821a272b6bdb4324f7694c2
Jose Luis Martinez <jlmartinez@capside.com>
2017-09-01 16:16:50 +0000

    Update some very old examples to recommended usage

--- examples/resource_list.pl
+++ examples/resource_list.pl
@@ -4,24 +4,10 @@ use warnings;
 use strict;
 
 use Azure;
-use Feature;
-use ResourceManagement;
 
-use Azure::Net::Caller;
+my $r = Azure->service('ResourceManagement', subscription_id => $ENV{AZURE_SUBSCRIPTION_ID});
 
-my $r = ResourceManagement->new(
-  caller => Azure::Net::Caller->new,
-  credentials => Azure::Credentials::AzureADClientCredentials->new(
-    tenant_id => $ENV{AZURE_TENANT_ID},
-    client_id => $ENV{AZURE_CLIENT_ID},
-    secret_id => $ENV{AZURE_SECRET_ID},
-  ),
-);
-
-my $ret = $r->ListResources(
-  'api-version' => '2016-02-01',
-  subscriptionId => $ENV{AZURE_SUBSCRIPTION_ID},
-);
+my $ret = $r->ListResources();
 
 use Data::Dumper;
 print Dumper($ret);
