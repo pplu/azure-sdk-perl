b01491fbb311ee6572cf61f5db6641d1f13d4ff5
Jose Luis Martinez <jlmartinez@capside.com>
2017-09-10 19:31:20 +0000

    Update CosmosDB

---
+++ auto-lib/Azure/CosmosDB/ListOperations.pm
@@ -0,0 +1,12 @@
+package Azure::CosmosDB::ListOperations;
+  use Moose;
+  use MooseX::ClassAttribute;
+
+  has 'api-version' => (is => 'ro', required => 1, isa => 'Str', default => '2015-04-08',
+    traits => [ 'Azure::ParamInQuery' ],
+  );
+
+  class_has _api_uri => (is => 'ro', default => '/providers/Microsoft.DocumentDB/operations');
+  class_has _returns => (is => 'ro', default => 'Azure::CosmosDB::ListOperationsResult');
+  class_has _api_method => (is => 'ro', default => 'GET');
+1;
