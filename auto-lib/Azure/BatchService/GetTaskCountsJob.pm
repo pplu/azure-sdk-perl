006d97316d8edaf0c053f6ce6e3ecbab97a8dbf6
Jose Luis Martinez <jlmartinez@capside.com>
2017-09-10 19:57:57 +0000

    BatchService API update

---
+++ auto-lib/Azure/BatchService/GetTaskCountsJob.pm
@@ -0,0 +1,27 @@
+package Azure::BatchService::GetTaskCountsJob;
+  use Moose;
+  use MooseX::ClassAttribute;
+
+  has 'api-version' => (is => 'ro', required => 1, isa => 'Str', default => '2017-06-01.5.1',
+    traits => [ 'Azure::ParamInQuery' ],
+  );
+  has 'client-request-id' => (is => 'ro', isa => 'Str',
+    traits => [ 'Azure::ParamInHeader' ],
+  );
+  has 'jobId' => (is => 'ro', required => 1, isa => 'Str',
+    traits => [ 'Azure::ParamInPath' ],
+  );
+  has 'ocp-date' => (is => 'ro', isa => 'Str',
+    traits => [ 'Azure::ParamInHeader' ],
+  );
+  has 'return-client-request-id' => (is => 'ro', isa => 'Bool',
+    traits => [ 'Azure::ParamInHeader' ],
+  );
+  has 'timeout' => (is => 'ro', isa => 'Int',
+    traits => [ 'Azure::ParamInQuery' ],
+  );
+
+  class_has _api_uri => (is => 'ro', default => '/jobs/{jobId}/taskcounts');
+  class_has _returns => (is => 'ro', default => 'Azure::BatchService::GetTaskCountsJobResult');
+  class_has _api_method => (is => 'ro', default => 'GET');
+1;
