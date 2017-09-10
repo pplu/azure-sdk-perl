c9b3b8d20e3c45e82b9489e5f681b88fe31b2cfe
Jose Luis Martinez <jlmartinez@capside.com>
2017-09-10 19:35:09 +0000

    DataLakeAnalyticsAccountManagement regenerated

--- auto-lib/Azure/DataLakeAnalyticsAccountManagement/DataLakeAnalyticsAccountListResult.pm
+++ auto-lib/Azure/DataLakeAnalyticsAccountManagement/DataLakeAnalyticsAccountListResult.pm
@@ -2,5 +2,5 @@ package Azure::DataLakeAnalyticsAccountManagement::DataLakeAnalyticsAccountListR
   use Moose;
 
   has 'nextLink' => (is => 'ro', isa => 'Str'  );
-  has 'value' => (is => 'ro', isa => 'ArrayRef[Azure::DataLakeAnalyticsAccountManagement::DataLakeAnalyticsAccount]'  );
+  has 'value' => (is => 'ro', isa => 'ArrayRef[Azure::DataLakeAnalyticsAccountManagement::DataLakeAnalyticsAccountBasic]'  );
 1;
