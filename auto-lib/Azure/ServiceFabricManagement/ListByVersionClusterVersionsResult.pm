c989ec46f3cc527f9b70ce1a803f52a190bb9b33
Jose Luis Martinez <jlmartinez@capside.com>
2017-09-28 17:58:33 +0000

    API update: ServiceFabricManagement

--- auto-lib/Azure/ServiceFabricManagement/ListByVersionClusterVersionsResult.pm
+++
@@ -1,7 +0,0 @@
-package Azure::ServiceFabricManagement::ListByVersionClusterVersionsResult;
-  use Moose;
-
-  has nextLink => (is => 'ro', isa => 'Str'  );
-  has value => (is => 'ro', isa => 'ArrayRef[Azure::ServiceFabricManagement::ClusterCodeVersionsResult]'  );
-
-1;
