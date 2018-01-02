c9f2b9127e16d7c33e9abe124ed56be94575323e
Jose Luis Martinez <jlmartinez@capside.com>
2018-01-02 16:30:31 +0000

    Refactor new_from_struct. Better code, handles more data types

--- lib/Azure/API/JsonResponse.pm
+++ lib/Azure/API/JsonResponse.pm
@@ -84,122 +84,38 @@ package Azure::API::JsonResponse;
   }
 
   sub new_from_struct {
-    my ($self, $class, $result) = @_;
-    my %args;
- 
-    if ($class->does('Azure::API::StrToObjMapParser')) {
-      return $self->handle_response_strtoobjmap($class, $result);
-    } elsif ($class->does('Azure::API::StrToNativeMapParser')) {
-      return $self->handle_response_strtonativemap($class, $result);
+    my ($self, $type, $value) = @_;
+
+    if ($type eq 'Bool') {
+      if    ($value eq 'true')  { return 1; } 
+      elsif ($value eq 'false') { return 0; }
+      elsif ($value == 1)       { return 1; }
+      elsif ($value == 0)       { return 0; }
+      else  { die "$value is strange for a boolean"; }
+    } elsif ($type eq 'Str' or $type eq 'Int' or $type eq 'Num') {
+      return $value;
+    } elsif ($type eq 'HashRef') {
+      return $value;
+    } elsif (my ($hash_inner) = ("$type" =~ m/^HashRef\[(.*)]$/)) {
+      return { map { ($_ => $self->new_from_struct($hash_inner, $value->{$_})) } grep { defined $value->{ $_ } } keys %$value };
+    } elsif (my ($array_inner) = ("$type" =~ m/^ArrayRef\[(.*)]$/)) {
+      return [ map { $self->new_from_struct($array_inner, $_) if (defined $_) } @$value ];
     } else {
-    foreach my $att ($class->meta->get_attribute_list) {
-      next if (not my $meta = $class->meta->get_attribute($att));
+      # is an object
+      Azure->load_class("$type");
 
-      my $key = $meta->does('Azure::API::Attribute::Trait::Unwrapped') ? $meta->xmlname : $att;
-      my $att_type = $meta->type_constraint;
+      my %o_constructor_params;
+      foreach my $att ($type->meta->get_attribute_list) {
+        next if (not my $meta = $type->meta->get_attribute($att));
+        my $att_type = $meta->type_constraint;
+        
+        my $key = $meta->does('Azure::LocationInResponse') ? $meta->location : $att;
+        my $value = $value->{ $key };
 
-      use Data::Dumper;
-      #print STDERR "GOING TO DO AN $att_type\n";
-      #print STDERR "VALUE: " . Dumper($result);
-      # We'll consider that an attribute without brackets [] isn't an array type
-      if ($att_type !~ m/\[.*\]$/) {
-        my $value = $result->{ $key };
-next if (not defined $value);
-        my $value_ref = ref($value);
-
-        if ($att_type =~ m/\:\:/) {
-          # Make the att_type stringify for module loading
-          Azure->load_class("$att_type");
-          if (defined $value) {
-            if (not $value_ref) {
-              $args{ $att } = $value;
-            } else {
-              my $att_class = $att_type->class;
-
-              if ($att_class->does('Azure::API::StrToObjMapParser')) {
-                $args{ $att } = $self->handle_response_strtoobjmap($att_class, $value);
-              } elsif ($att_class->does('Azure::API::StrToNativeMapParser')) {
-                $args{ $att } = $self->handle_response_strtonativemap($att_class, $value);
-              } elsif ($att_class->does('Azure::API::MapParser')) {
-                my $xml_keys = $att_class->xml_keys;
-                my $xml_values = $att_class->xml_values;
-
-                #TODO: handle in one place
-                if ($value_ref eq 'HASH') {
-                  if (exists $value->{ member }) {
-                    $value = $value->{ member };
-                  } elsif (exists $value->{ entry }) {
-                    $value = $value->{ entry  };
-                  } elsif (keys %$value == 1) {
-                    $value = $value->{ (keys %$value)[0] };
-                  } else {
-                    #die "Can't detect the item that has the array in the response hash";
-                  }
-                  $value_ref = ref($value);
-                }
-
-                $args{ $att } = $att_class->new(map { ($_->{ $xml_keys } => $_->{ $xml_values }) } @$value);
-              } else {
-                $args{ $att } = $self->new_from_struct($att_class, $value);
-              }
-            }
-          }
-        } else {
-          if (defined $value) {
-            if ($att_type eq 'Bool') {
-              if ($value eq 'true') {
-                $args{ $att } = 1;
-              } elsif ($value eq 'false') {
-                $args{ $att } = 0;
-              } elsif ($value == 1) {
-                $args{ $att } = 1;
-              } else {
-                $args{ $att } = 0;
-              }
-            } else {
-              $args{ $att } = $value;
-            }
-          }
-        }
-      } elsif (my ($type) = ($att_type =~ m/^ArrayRef\[(.*)\]$/)) {
-        my $value = $result->{ $att };
-        $value = $result->{ $key } if (not defined $value and $key ne $att);
-next if (not defined $value);
-        my $value_ref = ref($value);
-
-        if ($type =~ m/\:\:/) {
-          if (defined $value) {
-            Azure->load_class($type);
-
-            my $val = $value;
-            #use Data::Dumper;
-            #print Dumper($value_ref, $value);
-            die "Got a non-array value" if ($value_ref ne 'ARRAY');
-
-            if ($type->does('Azure::API::StrToObjMapParser')) {
-              $args{ $att } = [ map { $self->handle_response_strtoobjmap($type, $_) } @$val ];
-            } elsif ($type->does('Azure::API::StrToNativeMapParser')) {
-              $args{ $att } = [ map { $self->handle_response_strtonativemap($type, $_) } @$val ];
-            } elsif ($type->does('Azure::API::MapParser')) {
-              die "MapParser Type in an Array. Please implement me";
-            } else {
-              $args{ $att } = [ map { $self->new_from_struct($type, $_) } @$val ];
-            }
-          }
-        } else {
-          #use Data::Dumper;
-          #print 'NON-OBJECT-ARRAY' . Dumper($value, $value_ref);
-          if (defined $value){
-            if ($value_ref eq 'ARRAY') {
-              $args{ $att } = $value; 
-            } else {
-              die "Non-Array value";
-            }
-          }
-        }
+        $o_constructor_params{ $key } = $self->new_from_struct("$att_type", $value) if (defined $value);
       }
-    }
-    $class->new(%args);
+      
+      return $type->new(\%o_constructor_params);         
     }
   }
 
